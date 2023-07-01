08e0296b077ff1950c9b32f60cd7ac7448e00e61
James Otwane <127046977+ja259@users.noreply.github.com>
2023-07-01 08:33:43 -0400

    Add  src/backgroundImg.js

---
+++ note-app/src/backgroundImg.js
@@ -0,0 +1,23 @@
+import img0 from "./img/img0.jpg";
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.jpg";
+import img4 from "./img/img4.jpg";
+import img5 from "./img/img5.jpg";
+import img6 from "./img/img6.jpg";
+import img7 from "./img/img7.jpg";
+import img8 from "./img/img8.jpg";
+
+const images = [
+  { id: 0, src: img0 },
+  { id: 1, src: img1 },
+  { id: 2, src: img2 },
+  { id: 3, src: img3 },
+  { id: 4, src: img4 },
+  { id: 5, src: img5 },
+  { id: 6, src: img6 },
+  { id: 7, src: img7 },
+  { id: 8, src: img8 },
+];
+
+export default images;
