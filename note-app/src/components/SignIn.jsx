4780d1569c3dde43052cdc434525e79bd9855927
James Otwane <127046977+ja259@users.noreply.github.com>
2023-07-01 08:40:51 -0400

    Add  src/components/SignIn.jsx

---
+++ note-app/src/components/SignIn.jsx
@@ -0,0 +1,18 @@
+import firebase, { auth } from "../firebase.js";
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in
+      </button>
+    </>
+  );
+}
+
+export default SignIn;
